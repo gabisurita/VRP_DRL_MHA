9d78b05788b51e4c4a8990be9d049cd9eb4dfd97
Gabriela Surita <gabsurita@gmail.com>
2021-09-27 15:05:55 -0300

    cleanup

--- TensorFlow2/model.py
+++ TensorFlow2/model.py
@@ -1,47 +1,344 @@
 import tensorflow as tf
+import tensorflow.keras.backend as K
 
-from data import generate_data
-from encoder import GraphAttentionEncoder
-from decoder import DecoderCell
+from decoder_utils import TopKSampler, CategoricalSampler, Env
+import numpy as np
+
+
+class DotProductAttention(tf.keras.layers.Layer):
+    def __init__(
+        self, clip=None, return_logits=False, head_depth=16, inf=1e10, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.clip = clip
+        self.return_logits = return_logits
+        self.inf = inf
+        dk = tf.cast(head_depth, tf.float32)
+        self.scale = tf.math.sqrt(dk)
+
+    def call(self, x, mask=None):
+        """Q: (batch, n_heads, q_seq(=n_nodes or =1), head_depth)
+        K: (batch, n_heads, k_seq(=n_nodes), head_depth)
+        logits: (batch, n_heads, q_seq(this could be 1), k_seq)
+        mask: (batch, n_nodes, 1), e.g. tf.Tensor([[ True], [ True], [False]])
+        mask[:,None,None,:,0]: (batch, 1, 1, n_nodes) ==> broadcast depending on logits shape
+        [True] -> [1 * -np.inf], [False] -> [logits]
+        """
+        Q, K, V = x
+        logits = tf.matmul(Q, K, transpose_b=True) / self.scale
+
+        if self.clip is not None:
+            logits = self.clip * tf.math.tanh(logits)
+
+        if self.return_logits:
+            if mask is not None:
+                logits = tf.where(
+                    tf.transpose(mask, perm=(0, 2, 1)),
+                    tf.ones_like(logits) * (-np.inf),
+                    logits,
+                )
+            return logits
+
+        if mask is not None:
+            logits = tf.where(
+                mask[:, None, None, :, 0], tf.ones_like(logits) * (-np.inf), logits
+            )
+
+        probs = tf.nn.softmax(logits, axis=-1)
+        return tf.matmul(probs, V)
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        n_heads=8,
+        embed_dim=128,
+        clip=None,
+        return_logits=None,
+        need_W=None,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.n_heads = n_heads
+        self.embed_dim = embed_dim
+        self.head_depth = self.embed_dim // self.n_heads
+        if self.embed_dim % self.n_heads != 0:
+            raise ValueError("embed_dim = n_heads * head_depth")
+
+        self.need_W = need_W
+        self.attention = DotProductAttention(
+            clip=clip, return_logits=return_logits, head_depth=self.head_depth
+        )
+        if self.need_W:
+            # stdv = 1./tf.math.sqrt(tf.cast(embed_dim, tf.float32))
+            # init = tf.keras.initializers.RandomUniform(minval = -stdv, maxval = stdv)# init = tf.random_uniform_initializer(minval = -stdv, maxval= stdv)
+            self.Wk = tf.keras.layers.Dense(
+                self.embed_dim, use_bias=False
+            )  # torch.nn.Linear(embed_dim, embed_dim)
+            self.Wv = tf.keras.layers.Dense(self.embed_dim, use_bias=False)
+            self.Wq = tf.keras.layers.Dense(self.embed_dim, use_bias=False)
+            self.Wout = tf.keras.layers.Dense(self.embed_dim, use_bias=False)
+
+    def split_heads(self, T, batch):
+        """https://qiita.com/halhorn/items/c91497522be27bde17ce
+        T: (batch, n_nodes, self.embed_dim)
+        T reshaped: (batch, n_nodes, self.n_heads, self.head_depth)
+        return: (batch, self.n_heads, n_nodes, self.head_depth)
+        """
+        T = tf.reshape(T, (batch, -1, self.n_heads, self.head_depth))
+        return tf.transpose(T, perm=(0, 2, 1, 3))
+
+    def combine_heads(self, T, batch):
+        """T: (batch, self.n_heads, n_nodes, self.head_depth)
+        T transposed: (batch, n_nodes, self.n_heads, self.head_depth)
+        return: (batch, n_nodes, self.embed_dim)
+        """
+        T = tf.transpose(T, perm=(0, 2, 1, 3))
+        return tf.reshape(T, (batch, -1, self.embed_dim))
+
+    def call(self, x, mask=None):
+        """q, k, v = x
+        encoder arg x: [x, x, x]
+        shape of q: (batch, n_nodes, embed_dim)
+        output[0] - output[h_heads-1]: (batch, n_nodes, head_depth)
+        --> concat output: (batch, n_nodes, head_depth * h_heads)
+        return output: (batch, n_nodes, embed_dim)
+        """
+        Q, K, V = x
+        if self.need_W:
+            Q, K, V = self.Wq(Q), self.Wk(K), self.Wv(V)
+
+        batch = K.shape[0]
+        output = self.attention(
+            [self.split_heads(T, batch) for T in [Q, K, V]], mask=mask
+        )
+        output = self.combine_heads(output, batch)
+        if self.need_W:
+            return self.Wout(output)
+        return output
+
+
+class ResidualBlock_BN(tf.keras.layers.Layer):
+    def __init__(self, MHA, BN, **kwargs):
+        super().__init__(**kwargs)
+        self.MHA = MHA
+        self.BN = BN
+
+    def call(self, x, mask=None, training=True):
+        if mask is None:
+            return self.BN(x + self.MHA(x), training=training)
+        return self.BN(x + self.MHA(x, mask), training=training)
+
+
+class SelfAttention(tf.keras.layers.Layer):
+    def __init__(self, MHA, **kwargs):
+        super().__init__(**kwargs)
+        self.MHA = MHA
+
+    def call(self, x, mask=None):
+        return self.MHA([x, x, x], mask=mask)
+
+
+class EncoderLayer(tf.keras.layers.Layer):
+    def __init__(self, n_heads=8, FF_hidden=512, activation="relu", **kwargs):
+        super().__init__(**kwargs)
+        self.n_heads = n_heads
+        self.FF_hidden = FF_hidden
+        self.activation = activation
+        self.BN1 = tf.keras.layers.BatchNormalization(trainable=True)
+        self.BN2 = tf.keras.layers.BatchNormalization(trainable=True)
+
+    def build(self, input_shape):
+        self.MHA_sublayer = ResidualBlock_BN(
+            SelfAttention(
+                MultiHeadAttention(
+                    n_heads=self.n_heads, embed_dim=input_shape[2], need_W=True
+                )  # input_shape[2] = embed_dim = 128
+            ),
+            self.BN1,
+        )
+        self.FF_sublayer = ResidualBlock_BN(
+            tf.keras.models.Sequential(
+                [
+                    # tf.keras.layers.Dense(self.FF_hidden, use_bias = True, activation = self.activation, kernel_initializer = init, bias_initializer = init),
+                    # tf.keras.layers.Dense(input_shape[2], use_bias = True, kernel_initializer = init, bias_initializer = init)
+                    tf.keras.layers.Dense(
+                        self.FF_hidden, use_bias=True, activation=self.activation
+                    ),
+                    tf.keras.layers.Dense(input_shape[2], use_bias=True),
+                ]
+            ),
+            self.BN2,
+        )
+        super().build(input_shape)
+
+    def call(self, x, mask=None, training=True):
+        """arg x: (batch, n_nodes, embed_dim)
+        return: (batch, n_nodes, embed_dim)
+        """
+        return self.FF_sublayer(
+            self.MHA_sublayer(x, mask=mask, training=training), training=training
+        )
+
+
+class GraphAttentionEncoder(tf.keras.models.Model):
+    def __init__(self, embed_dim=128, n_heads=8, n_layers=3, FF_hidden=512):
+        super().__init__()
+        # stdv = 1./tf.math.sqrt(tf.cast(embed_dim, tf.float32))
+        # init = tf.keras.initializers.RandomUniform(minval = -stdv, maxval = stdv)
+        # self.init_W_depot = tf.keras.layers.Dense(embed_dim, use_bias = True, kernel_initializer = init, bias_initializer = init)# torch.nn.Linear(2, embedding_dim)
+        # self.init_W = tf.keras.layers.Dense(embed_dim, use_bias = True, kernel_initializer = init, bias_initializer = init)# torch.nn.Linear(3, embedding_dim)
+        self.init_W_depot = tf.keras.layers.Dense(
+            embed_dim, use_bias=True
+        )  # torch.nn.Linear(2, embedding_dim)
+        self.init_W = tf.keras.layers.Dense(
+            embed_dim, use_bias=True
+        )  # torch.nn.Linear(3, embedding_dim)
+        self.encoder_layers = [
+            EncoderLayer(n_heads, FF_hidden) for _ in range(n_layers)
+        ]
+
+    @tf.function
+    def call(self, x, mask=None, training=True):
+        """x[0] -- depot_xy: (batch, 2) --> embed_depot_xy: (batch, embed_dim)
+        x[1] -- customer_xy: (batch, n_nodes-1, 2)
+        x[2] -- demand: (batch, n_nodes-1)
+        --> concated_customer_feature: (batch, n_nodes-1, 3) --> embed_customer_feature: (batch, n_nodes-1, embed_dim)
+        embed_x(batch, n_nodes, embed_dim)
+
+        return: (node embeddings(= embedding for all nodes), graph embedding(= mean of node embeddings for graph))
+                =((batch, n_nodes, embed_dim), (batch, embed_dim))
+        """
+        x = tf.concat(
+            [
+                self.init_W_depot(x[0])[:, None, :],
+                self.init_W(tf.concat([x[1], x[2][:, :, None]], axis=-1)),
+            ],
+            axis=1,
+        )
+
+        for layer in self.encoder_layers:
+            x = layer(x, mask, training)
+
+        return (x, tf.reduce_mean(x, axis=1))
+
+
+class DecoderCell(tf.keras.models.Model):
+    def __init__(self, embed_dim=128, n_heads=8, clip=10.0, **kwargs):
+        super().__init__(**kwargs)
+
+        self.Wk1 = tf.keras.layers.Dense(
+            embed_dim, use_bias=False
+        )  # torch.nn.Linear(embed_dim, embed_dim, bias = False)
+        self.Wv = tf.keras.layers.Dense(embed_dim, use_bias=False)
+        self.Wk2 = tf.keras.layers.Dense(embed_dim, use_bias=False)
+        self.Wq_fixed = tf.keras.layers.Dense(embed_dim, use_bias=False)
+        self.Wout = tf.keras.layers.Dense(embed_dim, use_bias=False)
+        self.Wq_step = tf.keras.layers.Dense(embed_dim, use_bias=False)
+
+        self.MHA = MultiHeadAttention(
+            n_heads=n_heads, embed_dim=embed_dim, need_W=False
+        )
+        self.SHA = DotProductAttention(
+            clip=clip, return_logits=True, head_depth=embed_dim
+        )
+        # SHA ==> Single Head Attention, because this layer n_heads = 1 which means no need to spilt heads
+        self.env = Env
+
+    # @tf.function
+    def compute_static(self, node_embeddings, graph_embedding):
+        Q_fixed = self.Wq_fixed(graph_embedding[:, None, :])
+        K1 = self.Wk1(node_embeddings)
+        V = self.Wv(node_embeddings)
+        K2 = self.Wk2(node_embeddings)
+        return Q_fixed, K1, V, K2
+
+    @tf.function
+    def _compute_mha(self, Q_fixed, step_context, K1, V, K2, mask):
+        Q_step = self.Wq_step(step_context)
+        Q1 = Q_fixed + Q_step
+        Q2 = self.MHA([Q1, K1, V], mask=mask)
+        Q2 = self.Wout(Q2)
+        logits = self.SHA([Q2, K2, None], mask=mask)
+        return tf.squeeze(logits, axis=1)
+
+    # @tf.function
+    def call(self, x, encoder_output, return_pi=False, decode_type="sampling"):
+        """context: (batch, 1, 2*embed_dim+1)
+        tf.concat([graph embedding[:,None,:], previous node embedding, remaining vehicle capacity[:,:,None]], axis = -1)
+        encoder output
+        ==> graph embedding: (batch, embed_dim)
+        ==> node_embeddings: (batch, n_nodes, embed_dim)
+        previous node embedding: (batch, n_nodes, embed_dim)
+        remaining vehicle capacity(= D): (batch, 1)
+
+        mask: (batch, n_nodes, 1), dtype = tf.bool, [True] --> [-inf], [False] --> [logits]
+        context: (batch, 1, 2*embed_dim+1)
+
+        squeezed logits: (batch, n_nodes), logits denotes the value before going into softmax
+        next_node: (batch, 1), minval = 0, maxval = n_nodes-1, dtype = tf.int32
+        log_p: (batch, n_nodes) <-- squeezed logits: (batch, n_nodes), log(exp(x_i) / exp(x).sum())
+        """
+        node_embeddings, graph_embedding = encoder_output
+        Q_fixed, K1, V, K2 = self.compute_static(node_embeddings, graph_embedding)
+
+        env = Env(x, node_embeddings)
+        mask, step_context, D = env._create_t1()
+
+        selecter = {"greedy": TopKSampler(), "sampling": CategoricalSampler()}.get(
+            decode_type, None
+        )
+        log_ps = tf.TensorArray(
+            dtype=tf.float32,
+            size=0,
+            dynamic_size=True,
+            element_shape=(env.batch, env.n_nodes),
+        )
+        tours = tf.TensorArray(
+            dtype=tf.int32, size=0, dynamic_size=True, element_shape=(env.batch,)
+        )
+
+        for i in tf.range(env.n_nodes * 2):
+            logits = self._compute_mha(Q_fixed, step_context, K1, V, K2, mask)
+            log_p = tf.nn.log_softmax(logits, axis=-1)
+            next_node = selecter(log_p)
+            mask, step_context, D = env._get_step(next_node, D)
+
+            tours = tours.write(i, tf.squeeze(next_node, axis=1))
+            log_ps = log_ps.write(i, log_p)
+
+        pi = tf.transpose(tours.stack(), perm=(1, 0))
+        ll = env.get_log_likelihood(tf.transpose(log_ps.stack(), perm=(1, 0, 2)), pi)
+        cost = env.get_costs(pi)
+
+        if return_pi:
+            return cost, ll, pi
+        return cost, ll
+
 
 class AttentionModel(tf.keras.models.Model):
-	
-	def __init__(self, embed_dim = 128, n_encode_layers = 3, n_heads = 8, tanh_clipping = 10., FF_hidden = 512):
-		super().__init__()
-		
-		self.Encoder = GraphAttentionEncoder(embed_dim, n_heads, n_encode_layers, FF_hidden)
-		self.Decoder = DecoderCell(embed_dim, n_heads, tanh_clipping)
+    def __init__(
+        self,
+        embed_dim=128,
+        n_encode_layers=3,
+        n_heads=8,
+        tanh_clipping=10.0,
+        FF_hidden=512,
+    ):
+        super().__init__()
 
-	def call(self, x, training = True, return_pi = False, decode_type = 'greedy'):
-		encoder_output = self.Encoder(x, training = training)
-		decoder_output = self.Decoder(x, encoder_output, return_pi = return_pi, decode_type = decode_type)
-		if return_pi:
-			cost, ll, pi = decoder_output
-			return cost, ll, pi
-		cost, ll = decoder_output
-		return cost, ll
-		
-if __name__ == '__main__':
-	# tf.config.experimental_run_functions_eagerly(True)
-	model = AttentionModel()
-	dataset = generate_data(seed = 123)
-	return_pi = False
-	for i, data in enumerate(dataset.batch(6)):
-		output = model(data, decode_type = 'sampling', return_pi = return_pi)
-		if return_pi:
-			print(output[0])# cost: (batch,)
-			print(output[1])# ll: (batch,)
-			print(output[2])# pi: (batch, decode_step) # tour
-		else:
-			print(output[0])# cost: (batch,)
-			print(output[1])# ll: (batch,)
-		if i == 0:
-			break
+        self.Encoder = GraphAttentionEncoder(
+            embed_dim, n_heads, n_encode_layers, FF_hidden
+        )
+        self.Decoder = DecoderCell(embed_dim, n_heads, tanh_clipping)
 
-	# print('model.trainable_weights')
-	# for w in model.trainable_weights:
-	# 	print(w.name)
-	# 	print(w.numpy())
-
-	model.summary()
-
+    def call(self, x, training=True, return_pi=False, decode_type="greedy"):
+        encoder_output = self.Encoder(x, training=training)
+        decoder_output = self.Decoder(
+            x, encoder_output, return_pi=return_pi, decode_type=decode_type
+        )
+        if return_pi:
+            cost, ll, pi = decoder_output
+            return cost, ll, pi
+        cost, ll = decoder_output
+        return cost, ll
--- TensorFlow2/train.py
+++ TensorFlow2/train.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import tensorflow as tf
 from tqdm import tqdm
 from time import time
@@ -5,81 +7,145 @@ from time import time
 from model import AttentionModel
 from baseline import RolloutBaseline
 from data import generate_data
-from config import Config, load_pkl, file_parser
-
-# https://www.wandb.com/articles/wandb-customizing-training-loops-in-tensorflow-2
-
-def train(cfg, log_path = None):
-
-	def allocate_memory():
-	# https://qiita.com/studio_haneya/items/4dfaf2fb2ac44818e7e0
-		physical_devices = tf.config.experimental.list_physical_devices('GPU')
-		if len(physical_devices) > 0:
-			for k in range(len(physical_devices)):
-				tf.config.experimental.set_memory_growth(physical_devices[k], True)
-				print('memory growth:', tf.config.experimental.get_memory_growth(physical_devices[k]))
-		else:
-			print("Not enough GPU hardware devices available")
-
-	def rein_loss(model, inputs, bs, t):
-		L, ll = model(inputs, decode_type = 'sampling', training = True)
-		b = bs[t] if bs is not None else baseline.eval(inputs, L)
-		b = tf.stop_gradient(b)
-		return tf.reduce_mean((L - b) * ll), tf.reduce_mean(L)
-
-	def grad_func(model, inputs, bs, t):
-		with tf.GradientTape() as tape:
-			loss, L_mean = rein_loss(model, inputs, bs, t)
-		return loss, L_mean, tape.gradient(loss, model.trainable_variables)# model.trainable_variables == thita
-
-	allocate_memory()
-	model = AttentionModel(cfg.embed_dim, cfg.n_encode_layers, cfg.n_heads, cfg.tanh_clipping)
-	baseline = RolloutBaseline(model, cfg.task, cfg.weight_dir, cfg.n_rollout_samples, 
-							cfg.embed_dim, cfg.n_customer, cfg.warmup_beta, cfg.wp_epochs)
-	optimizer = tf.keras.optimizers.Adam(learning_rate = cfg.lr)
-	ave_loss = tf.keras.metrics.Mean()
-	ave_L = tf.keras.metrics.Mean()
-	
-	t1 = time()
-	for epoch in range(cfg.epochs):
-		dataset = generate_data(cfg.n_samples, cfg.n_customer)
-		
-		bs = baseline.eval_all(dataset)
-		bs = tf.reshape(bs, (-1, cfg.batch)) if bs is not None else None# bs: (cfg.batch_steps, cfg.batch) or None
-		
-		for t, inputs in enumerate(dataset.batch(cfg.batch)):
-			
-			loss, L_mean, grads = grad_func(model, inputs, bs, t)
-
-			grads, _ = tf.clip_by_global_norm(grads, 1.0)
-			optimizer.apply_gradients(zip(grads, model.trainable_variables))# optimizer.step
-			
-			ave_loss.update_state(loss)
-			ave_L.update_state(L_mean)
-			
-			if t%(cfg.batch_verbose) == 0:
-				t2 = time()
-				print('Epoch %d (batch = %d): Loss: %1.3f L: %1.3f, %dmin%dsec'%(
-					epoch, t, ave_loss.result().numpy(), ave_L.result().numpy(), (t2-t1)//60, (t2-t1)%60))
-				if cfg.islogger:
-					if log_path is None:
-						log_path = '%s%s_%s.csv'%(cfg.log_dir, cfg.task, cfg.dump_date)#cfg.log_dir = ./Csv/
-						with open(log_path, 'w') as f:
-							f.write('time,epoch,batch,loss,cost\n')
-					with open(log_path, 'a') as f:
-						f.write('%dmin%dsec,%d,%d,%1.3f,%1.3f\n'%(
-							(t2-t1)//60, (t2-t1)%60, epoch, t, ave_loss.result().numpy(), ave_L.result().numpy()))
-				t1 = time()
-
-		baseline.epoch_callback(model, epoch)
-		model.save_weights('%s%s_epoch%s.h5'%(cfg.weight_dir, cfg.task, epoch), save_format = 'h5')#cfg.weight_dir = ./Weights/
-
-		ave_loss.reset_states()
-		ave_L.reset_states()
-
-if __name__ == '__main__':
-	cfg = load_pkl(file_parser().path)
-	train(cfg)
 
 
-	
+def train(cfg, log_path=None):
+    def rein_loss(model, inputs, bs, t):
+        L, ll = model(inputs, decode_type="sampling", training=True)
+        b = bs[t] if bs is not None else baseline.eval(inputs, L)
+        b = tf.stop_gradient(b)
+        return tf.reduce_mean((L - b) * ll), tf.reduce_mean(L)
+
+    def grad_func(model, inputs, bs, t):
+        with tf.GradientTape() as tape:
+            loss, L_mean = rein_loss(model, inputs, bs, t)
+        return loss, L_mean, tape.gradient(loss, model.trainable_variables)
+
+    model = AttentionModel(
+        cfg.embed_dim, cfg.n_encode_layers, cfg.n_heads, cfg.tanh_clipping
+    )
+    baseline = RolloutBaseline(
+        model,
+        cfg.task,
+        cfg.weight_dir,
+        cfg.n_rollout_samples,
+        cfg.embed_dim,
+        cfg.n_customer,
+        cfg.warmup_beta,
+        cfg.wp_epochs,
+    )
+    optimizer = tf.keras.optimizers.Adam(learning_rate=cfg.lr)
+    ave_loss = tf.keras.metrics.Mean()
+    ave_L = tf.keras.metrics.Mean()
+
+    t1 = time()
+    for epoch in range(cfg.epochs):
+        dataset = generate_data(cfg.n_samples, cfg.n_customer)
+
+        bs = baseline.eval_all(dataset, cfg.batch)
+        bs = (
+            tf.reshape(bs, (-1, cfg.batch)) if bs is not None else None
+        )  # bs: (cfg.batch_steps, cfg.batch) or None
+
+        for t, inputs in enumerate(dataset.batch(cfg.batch)):
+
+            loss, L_mean, grads = grad_func(model, inputs, bs, t)
+
+            grads, _ = tf.clip_by_global_norm(grads, 1.0)
+            optimizer.apply_gradients(
+                zip(grads, model.trainable_variables)
+            )  # optimizer.step
+
+            ave_loss.update_state(loss)
+            ave_L.update_state(L_mean)
+
+            if t % (cfg.batch_verbose) == 0:
+                t2 = time()
+                print(
+                    "Epoch %d (batch = %d): Loss: %1.3f L: %1.3f, %dmin%dsec"
+                    % (
+                        epoch,
+                        t,
+                        ave_loss.result().numpy(),
+                        ave_L.result().numpy(),
+                        (t2 - t1) // 60,
+                        (t2 - t1) % 60,
+                    )
+                )
+                if cfg.islogger:
+                    if log_path is None:
+                        log_path = "%s%s_%s.csv" % (
+                            cfg.log_dir,
+                            cfg.task,
+                            cfg.dump_date,
+                        )  # cfg.log_dir = ./Csv/
+                        with open(log_path, "w") as f:
+                            f.write("time,epoch,batch,loss,cost\n")
+                    with open(log_path, "a") as f:
+                        f.write(
+                            "%dmin%dsec,%d,%d,%1.3f,%1.3f\n"
+                            % (
+                                (t2 - t1) // 60,
+                                (t2 - t1) % 60,
+                                epoch,
+                                t,
+                                ave_loss.result().numpy(),
+                                ave_L.result().numpy(),
+                            )
+                        )
+                t1 = time()
+
+        baseline.epoch_callback(model, epoch)
+        model.save_weights(
+            "%s%s_epoch%s.h5" % (cfg.weight_dir, cfg.task, epoch), save_format="h5"
+        )  # cfg.weight_dir = ./Weights/
+
+        ave_loss.reset_states()
+        ave_L.reset_states()
+
+
+@dataclass
+class Config:
+    mode: str = "train"
+    seed: int = 123
+    n_customer: int = 20
+    batch: int = 512
+    batch_steps: int = 2500
+    batch_verbose: int = 10
+    n_rollout_samples: int = 10_000
+    epochs: int = 20
+    embed_dim: int = 128
+    n_heads: int = 8
+    tanh_clipping: float = 10.0
+    n_encode_layers: int = 3
+    lr: float = 0.0001
+    warmup_beta: float = 0.8
+    wp_epochs: int = 1
+    islogger: bool = True
+    log_dir: str = "./Csv/"
+    weight_dir: str = "./Weights/"
+    pkl_dir: str = "./Pkl/"
+    cuda_dv: int = 0
+    task: str = "VRP20_train"
+    dump_date: str = "0726_03_41"
+    pkl_path: str = "./Pkl/VRP20_train.pkl"
+    n_samples: int = 1_280_000
+    # min_size
+    # max_size
+
+
+if __name__ == "__main__":
+    cfg = Config()
+    # cfg.embed_dim = 32
+    cfg.n_customer = 50
+    cfg.n_rollout_samples = 1000
+
+    # n_samples / batch must be integer.
+    cfg.n_samples = 100_000
+    cfg.batch = 500
+    cfg.batch_steps = 2500
+    cfg.learning_rate = 1e-3
+
+    print("Start train")
+    print(cfg.__dict__)
+    train(cfg)
